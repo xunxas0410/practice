642be4d0151fa0780ffb3d28ec4e442a18e10685
xunxas0410 <60127190+xunxas0410@users.noreply.github.com>
2021-11-22 16:07:20 +0800

    更新 slide

--- js/all.js
+++ js/all.js
@@ -1,5 +1,5 @@
 let menu =document.querySelector('#menu-btn');
-let navbar =document.querySelector('.navbar');
+let navbar =document.querySelector('.header .navbar');
 
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
@@ -7,7 +7,38 @@ menu.onclick = () => {
 }
 
 window.onscroll = () => {
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
 }
+
+
+let year = document.querySelector('.pricing .price-toggler .year');
+let month = document.querySelector('.pricing .price-toggler .month');
+let yearAmount = document.querySelectorAll('.pricing .price .year');
+let monthAmount = document.querySelectorAll('.pricing .price .month');
+
+year.onclick = () => {
+    year.classList.add('active');
+    month.classList.remove('active');
+    yearAmount.forEach(years => {
+        years.style.display = 'block';
+    });
+    monthAmount.forEach(months => {
+        months.style.display = 'none';
+    });
+
+}
+
+month.onclick = () => {
+    year.classList.remove('active');
+    month.classList.remove('active');
+    yearAmount.forEach(years => {
+        years.style.display = 'none';
+    });
+    monthAmount.forEach(months => {
+        months.style.display = 'block';
+    });
+
+}
+
 
